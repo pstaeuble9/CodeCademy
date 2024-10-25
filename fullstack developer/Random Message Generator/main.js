13a14902b10c187028e7b4090abb883b6a710932
Patrick Staeuble <pstaeuble9@gmail.com>
2024-10-25 13:12:01 -0400

    Generating quotes and JS

---
+++ fullstack developer/Random Message Generator/main.js
@@ -0,0 +1,9 @@
+let btn = document.getElementById('btn');
+let output = document.getElementById('output');
+let quotes = [
+    'Life is completely random',
+    'Love is stronger',
+    'The sea has neither meaning nor pity',
+    'Out of difficulties grow miracles',
+    'Gotta love animals'
+]
