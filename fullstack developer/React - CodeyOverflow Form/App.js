4d3f51483db6ab1d939edb3ba58a0a0c5838dec9
Patrick Staeuble <35965091+pstaeuble9@users.noreply.github.com>
2024-11-15 12:31:41 -0500

    Create App.js

---
+++ fullstack developer/React - CodeyOverflow Form/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {comments} from './commentData';
+import Card from './Card';
+
+function App () {
+  return (
+    comments.map(comment => <Card commentObject={comment} />)
+  )
+}
+export default App;
