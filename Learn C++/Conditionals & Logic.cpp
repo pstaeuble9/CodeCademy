0f93e06b879686e2b97909f2f3cdf12071871dc6
Patrick Staeuble <35965091+pstaeuble9@users.noreply.github.com>
2025-07-31 13:59:26 -0400

    Create Conditionals & Logic.cpp

---
+++ Learn C++/Conditionals & Logic.cpp
@@ -0,0 +1,27 @@
+if (x == 1) {
+    
+    weight = weight * 0.78;
+  
+  } else if (x == 2) {
+  
+    weight = weight * 0.39;
+  
+  } else if (x == 3) {
+    
+    weight = weight * 2.65;
+  
+  } else if (x == 4) {
+  
+    weight = weight * 1.17;
+  
+  } else if (x == 5) {
+    
+    weight = weight * 1.05;
+  
+  } else if (x == 6) {
+    
+    weight = weight * 1.23;
+  
+  }
+
+  std::cout << "\nYour weight: " << weight << "\n";
